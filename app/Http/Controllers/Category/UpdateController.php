76bb5eb2bc5bc4dbb900e043935f31d075b3c7d0
Qazar7 <qazar7@yandex.ru>
2022-12-04 22:44:14 +0300

    first save

---
+++ app/Http/Controllers/Category/UpdateController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Category;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UpdateController extends Controller
+{
+    public function __invoke(UpdateRequest $request, Category $category)
+    {
+        $data = $request->validated();
+        $category->update($data);
+
+        return view('category.show', compact($category));
+    }
+}
